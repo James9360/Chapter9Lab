487036627db103f4f3ae6b97b21cf3c94b3fd0ea
Dan Rusk <djrusk@gmail.com>
2015-09-16 22:54:44 -0400

    Update Question1 starter code

--- src/main/java/edu/carrollcc/cis232/Question1.java
+++ src/main/java/edu/carrollcc/cis232/Question1.java
@@ -1,13 +1,8 @@
 package edu.carrollcc.cis232;
 
-/**
- * Hello world!
- *
- */
-public class Question1 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Question1 {
+	public static void main(String[] args) {
+		//Question 1: Course Grades
+		//refer to readme.md
+	}
 }
